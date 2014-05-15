7088d2d7fcbc6071b4e357adcafb10a68431a0d5
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-05-15 12:41:56 +0200

    refactored content for config file

--- lib/indoctrinatr/tools/template_pack_scaffold.rb
+++ lib/indoctrinatr/tools/template_pack_scaffold.rb
@@ -1,20 +1,6 @@
 module Indoctrinatr
   module Tools
     class TemplatePackScaffold
-      DEFAULT_TEMPLATE_CONFIG_CONTENT = <<EOS
-template_name: '#{template_name}'
-template_description: 'Describe your template here'
-attributes:
-  -
-    name: first_attribute
-    data_type: Text
-    default_value: 'default value of first attribute'
-  -
-    name: second_attribute
-    data_type: Text
-    default_value: 'default value of second attribute'
-EOS
-
       attr_reader :template_name, :path_name, :config_file_path
 
       def initialize template_name
@@ -40,7 +26,7 @@ EOS
 
       def create_configuration_file
         File.open(config_file_path, 'w') do |config_file|
-          config_file.write DEFAULT_TEMPLATE_CONFIG_CONTENT
+          config_file.write default_template_config_content
         end
       end
 
@@ -49,6 +35,21 @@ EOS
 
       def config_file_path
         path_name.join 'configuration.yaml'
+      end
+
+      def default_template_config_content template_name
+"template_name: '#{template_name}'
+template_description: 'Describe your template here'
+attributes:
+  -
+    name: first_attribute
+    data_type: Text
+    default_value: 'default value of first attribute'
+  -
+    name: second_attribute
+    data_type: Text
+    default_value: 'default value of second attribute'
+"
       end
     end
   end

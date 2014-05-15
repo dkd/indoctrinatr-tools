5ed71cecb4c6cd4d948fc77a1741819d03edc08e
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-05-15 11:28:54 +0200

    refactored command args handling

--- lib/indoctrinatr/tools/template_pack_scaffold.rb
+++ lib/indoctrinatr/tools/template_pack_scaffold.rb
@@ -1,8 +1,14 @@
 module Indoctrinatr
   module Tools
     class TemplatePackScaffold
+      attr_reader :template_name, :path_name
+
       def initialize template_name
-        path_name = Pathname.new(Dir.pwd).join template_name
+        @template_name = template_name
+        @path_name = Pathname.new(Dir.pwd).join template_name
+      end
+
+      def call
         if Dir.exists? path_name
           raise "A folder with name #{template_name} already exists."
         else

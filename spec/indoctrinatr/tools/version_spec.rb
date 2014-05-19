8240066c1025c63530033e59c47c8cdc8798d412
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-05-19 14:57:39 +0200

    bumped version number

--- spec/indoctrinatr/tools/version_spec.rb
+++ spec/indoctrinatr/tools/version_spec.rb
@@ -1,5 +1,5 @@
 describe Indoctrinatr::Tools do
   it 'defines a VERSION constant' do
-    expect(Indoctrinatr::Tools::VERSION).to eq '0.0.1'
+    expect(Indoctrinatr::Tools::VERSION).to eq '0.0.2'
   end
 end

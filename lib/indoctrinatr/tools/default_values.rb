0b4af78613a21aa9849bdd8bb4f0d6b3533795d5
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-05-20 10:33:26 +0200

    actually read from hash

--- lib/indoctrinatr/tools/default_values.rb
+++ lib/indoctrinatr/tools/default_values.rb
@@ -3,8 +3,8 @@ module Indoctrinatr
     class DefaultValues
       def initialize attributes_as_hashes_in_array
         attributes_as_hashes_in_array.each do |attribute_hash|
-          (class << self; self; end).send(:define_method, attribute_hash.name.to_sym, -> { instance_variable_get("@#{attribute_hash.name}").to_s.to_latex })
-          instance_variable_set("@#{attribute_hash.name}", attribute_hash.default_value)
+          (class << self; self; end).send(:define_method, attribute_hash['name'].to_sym, -> { instance_variable_get("@#{attribute_hash['name']}").to_s.to_latex })
+          instance_variable_set("@#{attribute_hash['name']}", attribute_hash['default_value'])
         end
       end
 

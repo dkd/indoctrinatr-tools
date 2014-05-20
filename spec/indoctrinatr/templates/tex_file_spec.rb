7c23645b3746afc8f64a2b67f12a7324cc1b657f
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-05-20 14:47:58 +0200

    fixed spec

--- spec/indoctrinatr/templates/tex_file_spec.rb
+++ spec/indoctrinatr/templates/tex_file_spec.rb
@@ -1,6 +1,6 @@
 describe 'template.tex' do
   it 'should exist' do
-    template_file = Pathname.new(__FILE__).join '..', '..', '..', '..', 'lib', 'indoctrinatr', 'templates', 'template.tex'
+    template_file = Pathname.new(__FILE__).join '..', '..', '..', '..', 'lib', 'indoctrinatr', 'templates', 'template.tex.erb'
     expect(File.exists? template_file).to be_true
   end
 end

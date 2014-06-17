90874f71d7af91dc60d5aab722ca034e00e76d92
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-06-17 14:24:06 +0200

    use new file name

--- spec/indoctrinatr/templates/tex_file_spec.rb
+++ spec/indoctrinatr/templates/tex_file_spec.rb
@@ -1,4 +1,4 @@
-describe 'template.tex' do
+describe 'template.tex.erb' do
   it 'should exist' do
     template_file = Pathname.new(__FILE__).join '..', '..', '..', '..', 'lib', 'indoctrinatr', 'templates', 'template.tex.erb'
     expect(File.exists? template_file).to be_true

f24b5b940526dbb45062d69bf747eefe6e14c65b
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-07-01 10:12:41 +0200

    bumped version

--- lib/indoctrinatr/tools/version.rb
+++ lib/indoctrinatr/tools/version.rb
@@ -1,5 +1,5 @@
 module Indoctrinatr
   module Tools
-    VERSION = '0.0.8'
+    VERSION = '0.0.9'
   end
 end
--- spec/indoctrinatr/tools/version_spec.rb
+++ spec/indoctrinatr/tools/version_spec.rb
@@ -2,6 +2,6 @@ require 'indoctrinatr/tools/version'
 
 describe Indoctrinatr::Tools do
   it 'defines a VERSION constant' do
-    expect(Indoctrinatr::Tools::VERSION).to eq '0.0.8'
+    expect(Indoctrinatr::Tools::VERSION).to eq '0.0.9'
   end
 end

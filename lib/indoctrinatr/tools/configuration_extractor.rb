e396c1d637bf1a72163c7f30d24418699defcdf2
Luka Lüdicke <luka.luedicke@dkd.de>
2015-08-10 15:14:01 +0200

    expand configurationExtractor for template description

--- lib/indoctrinatr/tools/configuration_extractor.rb
+++ lib/indoctrinatr/tools/configuration_extractor.rb
@@ -11,12 +11,13 @@ module Indoctrinatr
         @template_pack_name = template_pack_name
       end
 
-      def call
+      def call # rubocop:disable Metrics/AbcSize
         config_file = YAML.load_file config_file_path
         configuration = TemplatePackConfiguration.new
         configuration.template_asset_path = assets_path.to_s
         configuration.default_file_name = "#{template_pack_name}_with_default_values.pdf"
         configuration.output_file_name = config_file.fetch('output_file_name', "#{template_pack_name}_with_default_values.pdf")
+        configuration.textual_description = config_file.fetch 'textual_description'
         configuration.attributes_as_hashes_in_array = config_file.fetch 'fields', []
         configuration
       end

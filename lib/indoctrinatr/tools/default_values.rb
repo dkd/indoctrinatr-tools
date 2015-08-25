3e4f6f1dd46f0839c87e8239f720cf4b27612a9b
Luka Lüdicke <luka.luedicke@dkd.de>
2015-08-25 18:38:19 +0200

    add pdf_with_field_names command (Ticket #64694)
    Squashed commits:
    use \texttt to format the fieldname prettier
    [e7a8d63] rename TemplateFieldNamesCreator class
    [5f9abe9] add comments and todos for document-with-fieldname feature
    [8e16ba5] output document with field names to different file name
    [7b13d5a] work around to_latex not escaping the < and > chars
    [bcbb65a] add command for VariableNamesParser
    [6db4f35] add class to set variable names as values

--- lib/indoctrinatr/tools/default_values.rb
+++ lib/indoctrinatr/tools/default_values.rb
@@ -49,6 +49,17 @@ module Indoctrinatr
           end
         end
       end
+
+      def _variable_names_as_values # for document with field names. TODO: separate class
+        @_configuration.attributes_as_hashes_in_array.each do |attribute_hash|
+          # work around to_latex not escaping the < and > chars
+          instance_variable_set("@_#{attribute_hash['name']}", "\\texttt{<\\%= #{attribute_hash['name'].to_latex} -\\%>}")
+          # TODO: differentiate between -%> and %> ?
+          define_singleton_method attribute_hash['name'].to_sym do
+            instance_variable_get("@_#{attribute_hash['name']}") # we escaped variable name already and don't want no escape for the other stuff
+          end
+        end
+      end
     end
   end
 end
---
+++ lib/indoctrinatr/tools/template_pack_fieldnames_creator.rb
@@ -0,0 +1,60 @@
+require 'indoctrinatr/tools/template_pack_helpers'
+require 'indoctrinatr/tools/default_values'
+require 'indoctrinatr/tools/configuration_extractor'
+require 'erubis'
+require 'to_latex'
+
+module Indoctrinatr
+  module Tools
+    class TemplatePackFieldnamesCreator # TODO: rename to field_names
+      include TemplatePackHelpers
+
+      attr_accessor :template_pack_name, :configuration, :field_name_values, :tex_file_content, :parsed_tex_file_content
+
+      def initialize template_pack_name
+        @template_pack_name = template_pack_name
+      end
+
+      def call
+        check_for_folder
+        read_config_file
+        read_tex_file
+        parse_tex_file
+        write_tex_file
+        compile_tex_file
+        # TODO: rename if necessary?
+        # TODO: delete the tex file?
+        show_success
+      end
+
+      private
+
+      def read_config_file
+        @configuration = ConfigurationExtractor.new(template_pack_name).call
+        @field_name_values = DefaultValues.new @configuration
+        field_name_values._variable_names_as_values
+      end
+
+      def read_tex_file
+        @tex_file_content = File.read tex_file_path
+      end
+
+      def parse_tex_file
+        @parsed_tex_file_content = Erubis::Eruby.new(@tex_file_content).result(field_name_values.retrieve_binding)
+      end
+
+      def write_tex_file
+        File.write tex_with_fieldname_values_file_path, parsed_tex_file_content
+      end
+
+      def compile_tex_file
+        args = ['-shell-escape', '-interaction', 'batchmode', tex_with_fieldname_values_file_path.to_s]
+        2.times { system('xelatex', *args) } # run two times for proper table-of-contents and page count handling. TODO: use latexmk
+      end
+
+      def show_success
+        puts "The template pack '#{template_pack_name}' has been successfully parsed to use the variable names"
+      end
+    end
+  end
+end

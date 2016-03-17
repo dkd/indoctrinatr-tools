c0d3b5013da8be284f5635850b5649c9f1f7c085
Luka Lüdicke <luka.luedicke@dkd.de>
2016-03-17 14:53:13 +0100

    let DocumentationContent inherit from ContentForTexFiles (Ticket #71305)
    not much to do here except deleting the retrieve_binding method

--- lib/indoctrinatr/tools/template_documentation_content.rb
+++ lib/indoctrinatr/tools/template_documentation_content.rb
@@ -1,3 +1,4 @@
+require 'indoctrinatr/tools/content_for_tex_files'
 require 'indoctrinatr/tools/template_pack_configuration'
 require 'indoctrinatr/tools/directory_helpers'
 require 'indoctrinatr/tools/template_documentation_source_file'
@@ -5,12 +6,14 @@ require 'indoctrinatr/tools/default_values'
 
 module Indoctrinatr
   module Tools
-    class TemplateDocumentationContent
+    class TemplateDocumentationContent < ContentForTexFiles
       include DirectoryHelpers
       include TemplatePackHelpers
 
       attr_accessor :template_pack_name, :list_of_files
 
+      # Overwrite the initialize method because the content is built up much more complex than for the other
+      # ContentForTexFile children.
       def initialize template_pack_name, configuration
         @configuration = configuration
         @template_pack_name = template_pack_name
@@ -21,10 +24,6 @@ module Indoctrinatr
         @default_values_pdf_path = pdf_with_default_values_file_path
         # should have been generated automatically, error if something went wrong with it:
         fail IOError, "template with default values does not exist in current directory. Run indoctrinatr pdf #{template_pack_name}" unless File.exist? @default_values_pdf_path # rubocop:disable Style/SignalException
-      end
-
-      def retrieve_binding
-        binding
       end
 
       private
@@ -48,6 +47,9 @@ module Indoctrinatr
           # usage of eval to execute the interpolation of a custom filename string with interpolation - e.g. a filename with the current date
         end
       end
+
+      # TODO: get path for PDF with field_names
+      # TODO: include PDF with field_names *if* it has been successfully generated
     end
   end
 end

19c6d5f93ad9dd4239c2252cb39b81673e11273a
dkdehenrich <eike.henrich@dkd.deh>
2022-11-17 09:23:43 +0100

    conversion of pdf_with_field_names and workflow commands to dry-cli

--- lib/indoctrinatr/tools/template_pack_default_values_compiler.rb
+++ lib/indoctrinatr/tools/template_pack_default_values_compiler.rb
@@ -10,7 +10,6 @@ module Indoctrinatr
       include Dry::Transaction
 
       step :setup
-      step :check_for_folder
       step :check_setup
       step :compile_tex_file
       step :rename_if_necessary
@@ -37,27 +36,36 @@ module Indoctrinatr
             tex_with_default_values_file_path: tex_with_default_values_file_path
           }
         )
+      rescue StandardError => e
+        Failure(e.message)
       end
+
       def check_setup(config)
         return Failure('Please specify a template pack name.') if config[:template_pack_name].empty? # rubocop:disable Style/SignalException
         return Failure ("A folder with name '#{config[:template_pack_name]}' does not exist.") unless Dir.exist? config[:path_name]# rub
         Success(config)
+      rescue StandardError => e
+        Failure(e.message)
       end
 
       def compile_tex_file(config)
-        make_pdf config[:tex_with_default_values_file_path], config[:pack_documentation_examples_dir_path], !config[:keep_aux_files]
+        make_pdf config[:tex_with_default_values_file_path], config[:pack_documentation_examples_dir_path], cleanup: !config[:keep_aux_files]
         Success(config)
+      rescue StandardError => e
+        Failure(e.message)
       end
 
       def rename_if_necessary(config) # rubocop:disable Metrics/AbcSize
         configuration = ConfigurationExtractor.new(config[:template_pack_name]).call # TODO: avoid repeated calling of the ConfigurationExtrator
         @default_values = DefaultValues.new configuration
-        return if @default_values.customized_output_file_name == @default_values.default_file_name
+        return Success() if @default_values.customized_output_file_name == @default_values.default_file_name
 
         custom_filepath = config[:pack_documentation_examples_dir_path] + @default_values.customized_output_file_name
         FileUtils.rm custom_filepath if File.exist? custom_filepath
         FileUtils.mv(pack_documentation_examples_dir_path + @default_values.default_file_name, custom_filepath)
         Success()
+      rescue StandardError => e
+        Failure(e.message)
       end
     end
   end
